2d6289658c48e494483c06ae0a248299d582d92c
Obiora Nwude <44972479+datcodeboy@users.noreply.github.com>
2021-11-23 12:56:42 +0000

    splash screen demo

---
+++ lib/splash.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:inventory_app/home.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigatetohome();
+  }
+
+  _navigatetohome() async {
+    await Future.delayed(Duration(milliseconds: 3000));
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (context) => MyHomePage(title: 'InventoryApp')));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.blue,
+          ),
+          Container(
+            child: Text(
+              'Splash Screen',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}
